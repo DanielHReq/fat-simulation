90dc672e4bc82910bf9a811d2ad17fc48235fd6d
Caio Lucci Schnor <caio.lucci@unesp.br>
2025-06-10 11:48:55 -0300

    Add files via upload

---
+++ fat.c
@@ -0,0 +1,132 @@
+#include "fat.h"
+#include "ds.h"
+#include <errno.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define SUPER 0
+#define DIR 1
+#define FAT 2
+
+#define SIZE 1024
+
+
+// Estrutura do Superbloco
+#define MAGIC_N           0xAC0010DE
+typedef struct{
+	int magic;
+	int number_blocks;
+	int n_fat_blocks;			//quantos blocos a FAT precisa
+	char empty[BLOCK_SIZE-3*sizeof(int)];
+} super;
+
+super sb;						//variável para receber o superbloco quando ele for para RAM
+
+// item do diretório
+#define MAX_LETTERS 6
+#define OK 1
+#define NON_OK 0
+typedef struct{
+	unsigned char used;			// usar OK ou NON_OK
+	char name[MAX_LETTERS+1];	// nome do arquivo, adionado do terminador de string
+	unsigned int length;		// tam do arq em bytes
+	unsigned int first;			// primeiro bloco do arquivo na FAT
+} dir_item;
+
+#define N_ITEMS (BLOCK_SIZE / sizeof(dir_item))	// quantos itens de diretório que cabem em um bloco
+dir_item dir[N_ITEMS];			//variável para receber o diretório quando ele for para a RAM
+
+// FAT
+#define FREE 0
+#define EOFF 1
+#define BUSY 2
+unsigned int *fat;				//Variável para trazer a FAT para RAM (tomar cuidado, alocação dinâmica)
+
+int mountState = 0;				// 0 = n montado, 1 = montado
+
+int fat_format(){ 
+  	return 0;
+}
+
+void fat_debug(){
+	printf("depurando\n");
+
+	super state_sb;						//Estado do Superbloco atual
+	dir_item state_dir[N_ITEMS];		//Estado do diretório no momento
+	unsigned int * state_FAT = NULL;	//Estado da FAT
+
+	//tratamento do superbloco
+	ds_read(SUPER, (char *)&state_sb);
+	printf("---- | Superbloco | ---- \n");
+	if(state_sb.magic == MAGIC_N){
+		printf("Número Mágico está OK \n");
+	}else{
+		printf("Valor do Número mágico incorreto! (x%x)\n", state_sb.magic);
+	}
+	printf("Quantidade de blocos: %d\n", state_sb.number_blocks);
+	printf("FAT está usando %d blocos\n", state_sb.n_fat_blocks);
+	
+	//leitura da FAT no disco
+	state_FAT = (unsigned int *)malloc(state_sb.number_blocks * sizeof(unsigned int));
+	if(state_FAT == NULL){
+		fprintf(stderr, "Erro: Falha na alocação de memória para a FAT durante depuração.\n");
+        return;
+	}
+
+	//percorrer os blocos da FAT
+	for(int i = 0; i < state_sb.n_fat_blocks; i++){
+		ds_read(FAT + i, (char *)&state_FAT[i * (BLOCK_SIZE/ sizeof(unsigned int))]);
+	}
+	
+	//Ler diretório 
+	ds_read(DIR, (char *)&state_dir);
+	
+	printf("---- | Diretório | ---- \n");
+	for(int i = 0; i < N_ITEMS; i++){
+		if(state_dir[i].used == OK){
+			printf("Nome do Arquivo: %s\n", state_dir[i].name);
+			printf("Tamanho: %d\n", state_dir[i].length);
+			printf("Blocos: ");
+
+			int blocos = state_dir[i].first;
+			while(blocos != EOFF && blocos != FREE && blocos < state_sb.number_blocks){
+				printf("%d ", blocos);
+				if(state_FAT[blocos] == EOFF) break;
+				blocos = state_FAT[blocos];
+			}
+			printf("\n");
+		}
+	}
+	printf("\n");
+	free(state_FAT);
+}
+
+int fat_mount(){
+  	return 0;
+}
+
+int fat_create(char *name){
+  	return 0;
+}
+
+int fat_delete( char *name){
+  	return 0;
+}
+
+int fat_getsize( char *name){ 
+	return 0;
+}
+
+//Retorna a quantidade de caracteres lidos
+int fat_read( char *name, char *buff, int length, int offset){
+	return 0;
+}
+
+//Retorna a quantidade de caracteres escritos
+int fat_write( char *name, const char *buff, int length, int offset){
+	return 0;
+}
